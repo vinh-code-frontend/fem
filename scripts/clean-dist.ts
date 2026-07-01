6f4d7b4afb67085719bed120d2276b59f22fae8c
Nguyen Le Vinh <vikvenus199x@gmail.com>
2026-07-01 22:32:58 +0700

    chore: update clean dist script

--- scripts/clean-dist.ts
+++ scripts/clean-dist.ts
@@ -1,7 +1,21 @@
 import fs from 'node:fs';
-const target = process.argv;
-// console.log(fs);
-console.log(target);
-fs.rmSync('dist', { recursive: true, force: true });
+import path from 'node:path';
+import chalk from 'chalk';
 
-throw new Error('hehe');
+const cleanDist = () => {
+  const target = process.argv;
+  const flag = target[2];
+
+  const dir = flag ? path.join('dist', flag) : 'dist';
+
+  if (!fs.existsSync(dir)) {
+    console.log(chalk.yellow(`Folder "${dir}" does not exist.`));
+    process.exit(1);
+  }
+  console.log(chalk.blue(`Cleaning folder "${dir}"..............`));
+  console.time(chalk.green(`Folder "${dir}" was removed in`));
+  fs.rmSync(dir, { recursive: true, force: true });
+  console.timeEnd(chalk.green(`Folder "${dir}" was removed in`));
+};
+
+cleanDist();
