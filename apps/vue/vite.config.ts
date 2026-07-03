88a57693846be34f228be9e83e8872b034f37023
Nguyen Le Vinh <79951174+vinh-code-frontend@users.noreply.github.com>
2026-07-03 17:34:40 +0700

    refactor: update style, font config packages

--- apps/vue/vite.config.ts
+++ apps/vue/vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig, mergeConfig } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { sharedConfig } from '../../vite.config.ts';
+import { fileURLToPath, URL } from 'node:url';
+import tailwindcss from '@tailwindcss/vite';
 
-export default mergeConfig(
-  sharedConfig,
-  defineConfig({
-    plugins: [vue()],
-    build: {
-      outDir: '../../dist/vue',
-      emptyOutDir: true,
+export default defineConfig({
+  plugins: [tailwindcss(), vue()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@resource': fileURLToPath(new URL('../../resources', import.meta.url)),
     },
-  }),
-);
+  },
+  build: {
+    outDir: '../../dist/vue',
+    emptyOutDir: true,
+  },
+});
--- packages/styles/index.d.ts
+++ packages/styles/index.d.ts
@@ -1,1 +1,1 @@
-declare module '@package/tailwindcss' {}
+declare module '@package/styles' {}
